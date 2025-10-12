d33023c7a26965147a03d8ee5aee60fdb5b449e5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-10-12 07:13:25 +0200

    [/] [Implementation] Changed algorithm to work by toggling at half-wave..
    .. well, originally we wanted to use CTC just to count half-wave and
    toggle pin but failed. So ended up using FastPwm for that.
    
    So no longer waves like "_~~". Only "_~" or "__~~". So less variability
    in frequencies. But we like symmetric square waves today.

--- src/me_FrequencyGenerator.cpp
+++ src/me_FrequencyGenerator.cpp
@@ -10,6 +10,22 @@
   Output is done on pin 5 which is related to that counter.
 */
 
+/*
+  Initial and final signal is LOW
+
+  Implementation guarantees half-wave temporal granularity.
+  You can rely that HIGH period won't be cut.
+
+  Possible wave forms: "__~~__~~_" and "__~~__~~___".
+  Because we don't know signal state at finishing.
+*/
+
+/*
+  Implementation guarantees 50% duty cycle
+
+  It means coarse real frequency but symmetric signal.
+*/
+
 #include <me_FrequencyGenerator.h>
 
 #include <me_BaseTypes.h>
@@ -19,7 +35,7 @@
 
 using namespace me_FrequencyGenerator;
 
-const TUint_1 OutputPinNumber = 5;
+const TUint_1 OutputPinNumber = 6;
 me_Pins::TOutputPin OutputPin;
 
 TBool me_FrequencyGenerator::SetFrequency_Hz(
@@ -37,27 +53,29 @@ TBool me_FrequencyGenerator::SetFrequency_Hz(
     It's like floating point numbers: more magnitude - less precision.
   */
 
-  const TUint_4 MaxFreq_Hz = (TUint_4) F_CPU / 8 / 2;
-  const TUint_4 MinFreq_Hz = (TUint_4) F_CPU / 8 / 255;
-  TUint_1 WaveDuration_Ut;
+  const TUint_4 Clock_Hz = F_CPU / 8;
+  const TUint_4 MaxFreq_Hz = Clock_Hz / 2;
+  const TUint_4 MinFreq_Hz = Clock_Hz / 255;
+  TUint_1 HalfWaveDuration_Ut;
   me_Counters::TCounter1 Counter;
 
   if ((Freq_Hz < MinFreq_Hz) || (Freq_Hz > MaxFreq_Hz))
     return false;
 
   OutputPin.Init(OutputPinNumber);
+  OutputPin.Write(0);
 
-  WaveDuration_Ut = (TUint_4) F_CPU / 8 / Freq_Hz;
+  HalfWaveDuration_Ut = (TUint_4) Clock_Hz / Freq_Hz / 2;
 
-  if (WaveDuration_Ut < 2)
+  if (HalfWaveDuration_Ut < 1)
     return false;
 
   Counter.Control->DriveSource = (TUint_1) me_Counters::TDriveSource_Counter1::None;
-  Counter.Control->PinActionOnMarkA = (TUint_1) me_Counters::TPinAction::None;
 
   Counter.SetAlgorithm(me_Counters::TAlgorithm_Counter1::FastPwm_ToMarkA);
-  *Counter.MarkA = WaveDuration_Ut - 1;
-  *Counter.MarkB = (WaveDuration_Ut - 1) / 2;
+  *Counter.MarkA = HalfWaveDuration_Ut - 1;
+
+  *Counter.Current = 0;
 
   return true;
 }
@@ -68,9 +86,8 @@ void me_FrequencyGenerator::StartFreqGen()
 
   TCounter1 Counter;
 
-  Counter.Control->PinActionOnMarkB = (TUint_1) TPinAction::Set;
-  *Counter.Current = 0;
   Counter.Control->DriveSource = (TUint_1) TDriveSource_Counter1::Internal_SlowBy2Pow3;
+  Counter.Control->PinActionOnMarkA = (TUint_1) me_Counters::TPinAction::Toggle;
 }
 
 void me_FrequencyGenerator::StopFreqGen()
@@ -87,12 +104,12 @@ void me_FrequencyGenerator::StopFreqGen()
 
     Stopping gracefully is not that simple.
 
-    After setting pin action on mark B to None, pin instantly
-    returns to what value it has before. We want to finish our
-    cycle before releasing it. Our cycle is (LOW HIGH).
+    After setting pin action on mark A to None, pin instantly
+    returns to what value it has before. We want to finish current
+    wave before releasing it.
 
-    To finish cycle we're waiting for "got mark A" flag.
-    This flag is likely is set now from previous iterations.
+    To finish wave we're waiting for "got mark A" flag.
+    This flag is likely set now from previous iterations.
     So we're clearing it first. And clearing in done
     by writing "true" to it. Yes, "true". Hardware magic!
   */
@@ -101,7 +118,7 @@ void me_FrequencyGenerator::StopFreqGen()
   while (!Counter.Status->GotMarkA);
 
   // Okay, we're at start of cycle, disconnect pin and power off counter
-  Counter.Control->PinActionOnMarkB = (TUint_1) TPinAction::None;
+  Counter.Control->PinActionOnMarkA = (TUint_1) TPinAction::None;
   Counter.Control->DriveSource = (TUint_1) TDriveSource_Counter1::None;
 }
 
@@ -110,4 +127,5 @@ void me_FrequencyGenerator::StopFreqGen()
   2025-09-14
   2025-09-15
   2025-10-10 Switched to counter 1 (from counter 3)
+  2025-10-12
 */
